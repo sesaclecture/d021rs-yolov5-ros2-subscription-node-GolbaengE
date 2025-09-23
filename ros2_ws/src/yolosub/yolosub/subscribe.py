06ab80692ff58c63af37650faebd2ee3bbd975d5
Sung <xaqxaq@naver.com>
2025-09-23 18:51:37 +0900

    과제 제출합니다

--- ros2_ws/src/yolosub/yolosub/subscribe.py
+++ ros2_ws/src/yolosub/yolosub/subscribe.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 import rclpy
-
+from rclpy.node import Node
+from std_msgs.msg import String
 
 class Subscriber(Node):
     def __init__(self):
         # TODO: Implement node initialization
+        super().__init__('subscriber_node')
         # TODO: Register callback
-        pass
+        self.subscription = self.create_subscription(
+            String,
+            'chatter',
+            self.listener_callback,
+            10
+        )
+        self.subscription
 
     def listener_callback(self, msg):
         # TODO: Implemnt a callback
-        pass
-
+        self.get_logger().info(f'I heard: "{msg.data}"')
 
 def main(args=None):
     rclpy.init(args=args)
